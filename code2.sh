12e4521198dba6536f421a6e760160080962d1a5
indo <indo>
2025-02-25 17:12:25 -0500

    code2

---
+++ code2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "brahmendra :: hi suajn "
+echo "sujan : chepu ra brahmendra "
+echo "brahmendra : i need 50k please send in phone pay "
+echo "sujan : i only have 5rs in my phone pay "
